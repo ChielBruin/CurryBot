b1bb30308f6867c3a9a3491b598a9a51b89fc3b1
Chiel Bruin <git@bruin.ch>
2019-12-11 20:59:30 +0100

    Added RSS actions

--- src/handlers/action/__init__.py
+++ src/handlers/action/__init__.py
@@ -1,5 +1,7 @@
 from .message import SendTextMessage, SendHTMLMessage, SendMarkdownMessage
 from .forward import Forward
 from .makeAdmin import MakeSenderBotAdmin
+from .youtube import YtPlaylistAppend
+from .rss import SendRSS, SendReddit
 
-__all__ = ['SendTextMessage', 'SendHTMLMessage', 'SendMarkdownMessage', 'MakeSenderBotAdmin', 'Forward']
+__all__ = ['SendTextMessage', 'SendHTMLMessage', 'SendMarkdownMessage', 'MakeSenderBotAdmin', 'Forward', 'YtPlaylistAppend', 'SendRSS', 'SendReddit']
