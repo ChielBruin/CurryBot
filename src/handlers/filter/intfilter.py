afefc3eb8062261efe33c94697d26a35665668f9
Chiel Bruin <git@bruin.ch>
2019-12-18 13:17:54 +0100

    Fixed configconversation for intfilter

--- src/handlers/filter/intfilter.py
+++ src/handlers/filter/intfilter.py
@@ -1,4 +1,5 @@
 import re
+from telegram import InlineKeyboardButton
 
 from ..messageHandler import MessageHandler
 from exceptions       import FilterException
@@ -53,25 +54,32 @@ class IntFilter (MessageHandler):
     @classmethod
     def create(cls, stage, data, arg):
         buttons = [[
-            InlineKeyboardButton(text='>',  callback_data='>'),
-            InlineKeyboardButton(text='<',  callback_data='<'),
-            InlineKeyboardButton(text='>=', callback_data='>='),
-            InlineKeyboardButton(text='<=', callback_data='<='),
-            InlineKeyboardButton(text='==', callback_data='=='),
-            InlineKeyboardButton(text='!=', callback_data='!=')
+            InlineKeyboardButton(text='>',  callback_data='b_>'),
+            InlineKeyboardButton(text='<',  callback_data='b_<'),
+            InlineKeyboardButton(text='>=', callback_data='b_>='),
+            InlineKeyboardButton(text='<=', callback_data='b_<='),
+            InlineKeyboardButton(text='==', callback_data='b_=='),
+            InlineKeyboardButton(text='!=', callback_data='b_!=')
         ]]
         if stage == 0:
             return (1, None, Send('What comparison operator should be used?', buttons=buttons))
         elif stage == 1:
             if isinstance(arg, str):
-                return (2, arg, Send('Which value should be at the right hand side of the comparison?'))
+                return (2, arg[2:], Send('Which value should be at the right hand side of the comparison?'))
             else:
                 return (1, None, Send('No, thats not the right button......', buttons=buttons))
         elif stage == 2:
-            if re.match(r'-?[\d]+', arg.text):
-                return (-1, None, Done(IntFilter(data, int(arg.text)))
+            if re.match(r'-?[\d]+$', arg.text):
+                return (3, ((data, int(arg.text)), []), AskChild())
             else:
                 return (2, data, Send('That is not an integer, examples are 1, 2, -4 and 112'))
+        elif stage == 3:
+            if isinstance(arg, MessageHandler):
+                data[1].append(arg)
+                return (3, data, AskChild())
+            else:
+                (comp, val), children = data
+                return (-1, None, Done(IntFilter(comp, val, children)))
         else:
             print(stage, data, arg)
             raise CreateException('Invalid create state for SenderIsBotAdmin')
